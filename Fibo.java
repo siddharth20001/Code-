7b80385c7e28ad3e2ab50db13c342f27e10a7fed
siddharth20001 <113451365+siddharth20001@users.noreply.github.com>
2022-11-01 21:53:43 +0530

    Create Fibo.java

---
+++ Fibo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Test {
+    public static void main(String[] args) {
+      int a=0,b=1,c=0;
+      System.out.print(a+" "+b+" ");
+      for(int i=0;i<10;i++)
+      {
+          c=a+b;
+          System.out.print(c+" ");
+          a=b;
+          b=c;
+      }
+    }
+}
