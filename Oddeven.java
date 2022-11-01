f7ee6c15df95882fe8ee764327bff30639297b6a
siddharth20001 <113451365+siddharth20001@users.noreply.github.com>
2022-11-01 21:44:34 +0530

    Create Oddeven.java

---
+++ Oddeven.java
@@ -0,0 +1,9 @@
+class Test {
+    public static void main(String[] args) {
+        int n=18;
+        if(n%2==0)
+        System.out.println("Even");
+        else
+        System.out.println("Odd");
+    }
+}
